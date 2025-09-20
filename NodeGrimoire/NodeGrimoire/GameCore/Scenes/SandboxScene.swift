b809d63ecfe6bc803f3457ba1af6f77c3bb5ccc1
Victor Vasconcelos <vfvasconcelos3010@gmail.com>
2025-09-20 19:43:13 -0300

    feat: add sandbox scene

---
+++ NodeGrimoire/NodeGrimoire/GameCore/Scenes/SandboxScene.swift
@@ -0,0 +1,18 @@
+//
+//  SandboxScene.swift
+//  NodeGrimoire
+//
+//  Created by Victor Vasconcelos on 20/09/25.
+//
+
+import SpriteKit
+import BehindGameKit
+
+class SandboxScene: SKGameScene {
+    public static func create(withSize size: CGSize = .init(width: 1920, height: 1080)) -> SandboxScene {
+        let scene = SandboxScene(size: size)
+        scene.scaleMode = .aspectFill
+        scene.anchorPoint = .centered
+        return scene
+    }
+}
---
+++ NodeGrimoire/NodeGrimoire/GameCore/Views/SandboxView.swift
@@ -0,0 +1,19 @@
+//
+//  SandboxView.swift
+//  NodeGrimoire
+//
+//  Created by Victor Vasconcelos on 20/09/25.
+//
+
+import SwiftUI
+import SpriteKit
+
+struct SandboxView: View {
+    var body: some View {
+        SpriteView(scene: SandboxScene.create())
+    }
+}
+
+#Preview {
+    SandboxView()
+}
