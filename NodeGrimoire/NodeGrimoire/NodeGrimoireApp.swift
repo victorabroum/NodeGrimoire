6edf8b5aa381d1bc1fed941b61dc0e2559dd935f
Victor Vasconcelos <vfvasconcelos3010@gmail.com>
2025-09-20 19:28:56 -0300

    start project

---
+++ NodeGrimoire/NodeGrimoire/NodeGrimoireApp.swift
@@ -0,0 +1,17 @@
+//
+//  NodeGrimoireApp.swift
+//  NodeGrimoire
+//
+//  Created by Victor Vasconcelos on 20/09/25.
+//
+
+import SwiftUI
+
+@main
+struct NodeGrimoireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
